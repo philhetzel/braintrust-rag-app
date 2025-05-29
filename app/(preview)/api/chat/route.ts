a18d34b035a9e83bb631b13cc58e6ead113b52a2
Phil Hetzel <phetzel1@protonmail.com>
2025-05-29 10:23:29 -0400

    pirate mode

--- app/(preview)/api/chat/route.ts
+++ app/(preview)/api/chat/route.ts
@@ -10,7 +10,7 @@ async function getPrompt() {
   const prompt = await loadPrompt({
     projectName: "PhilScratchArea",
     slug: "embedded-prompt",
-    version: "116ae465d365e1c0"
+    version: "0217e596f8bed565"
   });
   
   const prompt_obj = prompt.build('')
