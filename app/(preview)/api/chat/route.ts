1ea0cb72ad03e950abdf84980219f2a4c9236462
Phil Hetzel <phetzel1@protonmail.com>
2025-05-29 21:45:03 -0400

    changing back to normal

--- app/(preview)/api/chat/route.ts
+++ app/(preview)/api/chat/route.ts
@@ -10,7 +10,7 @@ async function getPrompt() {
   const prompt = await loadPrompt({
     projectName: "PhilScratchArea",
     slug: "embedded-prompt",
-    version: "3b43a76101a70ca8"
+    version: "116ae465d365e1c0"
   });
   
   const prompt_obj = prompt.build('')
