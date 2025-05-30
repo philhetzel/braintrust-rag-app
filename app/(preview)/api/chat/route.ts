fe087f9961bfa3cb3c1c0dbc1866701ed910a663
Phil Hetzel <phetzel1@protonmail.com>
2025-05-30 08:08:45 -0400

    make normal again

--- app/(preview)/api/chat/route.ts
+++ app/(preview)/api/chat/route.ts
@@ -13,7 +13,7 @@ async function getPrompt() {
   const prompt = await loadPrompt({
     projectName: "PhilScratchArea",
     slug: "embedded-prompt",
-    version: "038378664fc62eb5"
+    version: "74621f94c8c9631e"
   });
   
   const prompt_obj = prompt.build('')
